ebc15df8fb8f30af2de83e80f978155318c5402e
Archie-study <archiestudy12@gmail.com>
2024-09-03 16:44:36 +0700

    Update CheckEvenOdd code

--- src/main/java/CheckEvenOdd.java
+++ src/main/java/CheckEvenOdd.java
@@ -6,9 +6,12 @@ public class CheckEvenOdd {
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
         if (num % 2 == 0) {
-            System.out.println(num + " is even.");
+//            System.out.println(num + " is even.");
+            System.out.println(true);
         } else {
-            System.out.println(num + " is odd.");
+
+//            System.out.println(num + " is odd.");
+            System.out.println(false);
         }
     }
 }
